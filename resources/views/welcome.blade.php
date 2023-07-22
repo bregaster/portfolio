2c06888b60f1c3edad43d352588a3d53531e31d0
bregaster <bregasster@gmail.com>
2023-07-22 15:54:06 +0700

    hotjar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <title>Bregaster</title>
-    <script type='text/javascript'>
-        window.smartlook||(function(d) {
-    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
-    })(document);
-    smartlook('init', '9db5cec857fd1a60218fe022d9240c1178447225', { region: 'eu' });
+    <!-- Hotjar Tracking Code for https://bregaster.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3583998,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
